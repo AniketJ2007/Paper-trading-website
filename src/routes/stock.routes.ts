1f81c3519ebb8f351eecb43eacf6318596d1061f
AniketJ2007 <anijoshi1300@gmail.com>
2026-01-26 19:36:11 +0530

    Routine changes

--- src/routes/stock.routes.ts
+++ src/routes/stock.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware";
 import { addOption, BuyNormal, SellNormal } from "../controllers/stock.controller";
-import { stockData } from "../controllers/apidata.controller";
+import { getFrontData, stockData } from "../controllers/apidata.controller";
 const router=Router()
 router.route('/stockdata').post(stockData)
+router.route('/getfront').get(getFrontData)
 router.route('/buynormal').post(verifyJwt,BuyNormal)
 router.route('/sellnormal').post(verifyJwt,SellNormal)
 router.route('/limitorder').post(verifyJwt,addOption)
+
 
 
 export default router
